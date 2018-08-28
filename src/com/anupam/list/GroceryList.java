b19adee4b56d751b49900c486f019c7f6a2a3b01
ANUPAM DUBE <dubey.anupam@hotmail.com>
2018-08-29 02:10:51 +0530

    2nd commit

--- src/com/anupam/list/GroceryList.java
+++ src/com/anupam/list/GroceryList.java
@@ -1,7 +1,18 @@
 package com.anupam.list;
 
+import java.util.ArrayList;
+
 /**
  * Created by ANUPAM DUBE on Aug, 2018
  **/
 public class GroceryList {
+    private ArrayList groceryList = new ArrayList();
+
+    public GroceryList(ArrayList groceryList) {
+        this.groceryList = groceryList;
+    }
+
+    public ArrayList getGroceryList() {
+        return groceryList;
+    }
 }
